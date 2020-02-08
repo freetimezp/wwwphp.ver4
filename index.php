9555d74e6369619d62db49ec2edf4ec09de4b1e8
freetimezp <kraftukrnet@ukr.net>
2020-02-08 20:15:31 +0200

    PhP.Practic.4

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+<!-- main CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PhP-4</title>
+</head>
+<body>
+
+
+
+
+<!-- Optional JavaScript -->
+
+<!-- main JS -->
+<script src="js/script.js" ></script>
+
+
+</body>
+</html>
